01e40667153ecd3681446421a4cae2fe8bee565b
Fadddddd <fadelhannachi@gmail.com>
2023-11-18 11:07:28 -0500

    working on front

--- merchandise/views.py
+++ merchandise/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def accueil(request):
     return render (request, 'merchandise/header.html')
+
+def contact(request):
+    return render (request, 'merchandise/contact.html')
