e7f01cc87f40ded844b565e51ae171f181b8f82b
Fadddddd <fadelhannachi@gmail.com>
2023-11-19 20:41:45 -0500

    db + adding products + display

--- merchandise/models.py
+++ merchandise/models.py
@@ -1,28 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
-    libelle=models.CharField(max_length=255)
+    libelle=models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self) -> str:
-        return self.category
+        return self.libelle
 
     class Meta:
         verbose_name_plural="Categories"
 
 class Product(models.Model):
-    libelle=models.CharField(max_length=100)
+    libelle=models.CharField(max_length=100, blank=True, null=True)    
     description=models.CharField(max_length=200)
-    price=models.DecimalField(max_digits=5,decimal_places=2)
+    price=models.DecimalField(max_digits=5, decimal_places=2)
     image=models.ImageField(upload_to='merchandise_images', blank=True, null=True)
-    libelle_category=models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
-    author=models.ForeignKey(User, related_name="Products", on_delete=models.CASCADE)
-    created_on=models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural="Products"
+    categorie=models.ManyToManyField(Category)
 
     def __str__(self):
         return self.libelle
--- merchandise/views.py
+++ merchandise/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from merchandise.models import Category, Product
+
 def accueil(request):
-    return render (request, 'merchandise/header.html')
+    products=Product.objects.all()
+    categories=Category.objects.all()
+    return render (request, 'merchandise/index.html',
+    {'products':products,'categories':categories})
 
 def contact(request):
     return render (request, 'merchandise/contact.html')
